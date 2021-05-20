af2747a370373f5ba789a632581876082a74ce75
Diagq <76496452+Diagq@users.noreply.github.com>
2021-05-20 21:51:52 +0200

    Update leerFicheroXML.js

--- Ejercicio2/leerFicheroXML.js
+++ Ejercicio2/leerFicheroXML.js
@@ -3,7 +3,7 @@ function gestionarFicheroXML(xmlDoc){
 	let capaVacia = document.querySelector("#ficheroXML")
 	let dcha = xmlDoc.querySelectorAll("derecha")
 	let izqda = xmlDoc.querySelectorAll("izquierda")
-	for(let i=0; i<dcha.length&&i<izqda.length; i++)
+	for(let i=0; i<dcha.length&&izqda.length; i++)
 		capaVacia.innerHTML = capaVacia.innerHTML + "<p class='dcha'>" + dcha[i].textContent + "</p>"
 		capaVacia.innerHTML = capaVacia.innerHTML + "<p class='izqda'>" + izqda[i].textContent + "</p>"
 	
