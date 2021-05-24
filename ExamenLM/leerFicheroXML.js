061e6ca9b2545d9e61d7ff296523e247313baded
Diagq <76496452+Diagq@users.noreply.github.com>
2021-05-24 09:26:39 +0200

    Update leerFicheroXML.js

--- ExamenLM/leerFicheroXML.js
+++ ExamenLM/leerFicheroXML.js
@@ -3,8 +3,8 @@ function gestionarFicheroXML(xmlDoc){
 	let impares = xmlDoc.querySelectorAll("impar")
 	let pares = xmlDoc.querySelectorAll("par")
 	for(let i=0; i<impares.length&&pares.length; i++){
-		MouseEvent=Over capaVacia.innerHTML = capaVacia.innerHTML + "<p class ='impar'>" + impares[i].textContent + "</p>"
-		MouseEvent=Over capaVacia.innerHTML = capaVacia.innerHTML + "<p class ='par'>" + pares[i].textContent + "</p>"
+		MouseEvent=('Over') capaVacia.innerHTML = capaVacia.innerHTML + "<p class ='impar'>" + impares[i].textContent + "</p>"
+		MouseEvent=('Over') capaVacia.innerHTML = capaVacia.innerHTML + "<p class ='par'>" + pares[i].textContent + "</p>"
 	}
 }
 
